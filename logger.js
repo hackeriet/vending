1f3f9f84b24aeba6fa32c2ce86342a8bfee5e6b1
Stig Otnes Kolstad <stig@stigok.com>
2018-05-08 23:33:42 +0200

    Log level debug by default

--- logger.js
+++ logger.js
@@ -1,6 +1,7 @@
 const winston = require('winston')
 
 const logger = new winston.Logger({
+  level: 'debug',
   transports: [
     new (winston.transports.Console)({
       timestamp: () => Date.now(),
