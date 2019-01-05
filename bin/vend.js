35f5db0320ba7b369e452d96aed9a6ec1a77c170
Stig Otnes Kolstad <stig@stigok.com>
2019-01-05 23:42:06 +0100

    Update name of Kraftstoff

--- bin/vend.js
+++ bin/vend.js
@@ -22,7 +22,7 @@ const BUTTON_PINS = [7, 2, 0, 3, 4]
 const products = [
   { name: 'Club Mate', price: 35 },
   { name: 'Club Mate', price: 35 },
-  { name: 'Kraftstoff IceT', price: 35},
+  { name: 'Kraftstoff', price: 35 },
   { name: 'Guayaki Yerba Mate Orange', price: 35 },
   { name: 'Tuborg', price: 35 }
 ]
